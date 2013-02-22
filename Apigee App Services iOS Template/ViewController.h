f5fba7f11782c85c7f201b52a5e039109c551df9
Tim Anglade <timanglade@gmail.com>
2013-02-22 10:35:54 -0500

    Initial commit: working empty app

---
+++ Apigee App Services iOS Template/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Apigee App Services iOS Template
+//
+//  Created by Tim Anglade on 2/22/13.
+//  Copyright (c) 2013 Apigee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Apigee App Services iOS Template/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Apigee App Services iOS Template
+//
+//  Created by Tim Anglade on 2/22/13.
+//  Copyright (c) 2013 Apigee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
